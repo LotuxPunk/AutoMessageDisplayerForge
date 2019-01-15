60e6d2e4a08739e31053fd596790c7b34f986b2a
LotuxPunk <clement@vandendaelen.com>
2019-01-15 12:32:04 +0100

    Initial commit

---
+++ src/main/java/com/vandendaelen/automessagedisplayer/AutoMessageDisplayer.java
@@ -0,0 +1,26 @@
+package com.vandendaelen.automessagedisplayer;
+
+import com.vandendaelen.automessagedisplayer.utils.Reference;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION.VERSION, dependencies = Reference.DEP, acceptableRemoteVersions = "*")
+public class AutoMessageDisplayer {
+    private static Logger logger;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
