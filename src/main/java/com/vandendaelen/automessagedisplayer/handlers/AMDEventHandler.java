237bfdc6c7e9d279e43fcb9fd069fc00b84d6ade
LotuxPunk <clement@vandendaelen.com>
2019-01-19 16:14:59 +0100

    forge update checker stuff

--- src/main/java/com/vandendaelen/automessagedisplayer/handlers/AMDEventHandler.java
+++ src/main/java/com/vandendaelen/automessagedisplayer/handlers/AMDEventHandler.java
@@ -4,11 +4,19 @@ import com.vandendaelen.automessagedisplayer.AutoMessageDisplayer;
 import com.vandendaelen.automessagedisplayer.config.AMDConfig;
 import com.vandendaelen.automessagedisplayer.utils.PlayerHelper;
 import com.vandendaelen.automessagedisplayer.utils.Reference;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.event.ClickEvent;
+import net.minecraft.util.text.event.HoverEvent;
+import net.minecraftforge.common.ForgeVersion;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import scala.collection.immutable.List;
 
+import java.util.Arrays;
 import java.util.Random;
 
 @Mod.EventBusSubscriber(modid = Reference.MOD_ID)
@@ -43,6 +51,22 @@ public class AMDEventHandler {
             if (countdown >= END_COUNTDOWN){
                 messageDisplayer();
                 countdown = 0;
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerConnect(PlayerEvent.PlayerLoggedInEvent event){
+        if (AMDConfig.GENERAL.updateChecker){
+            String[] admins = event.player.getServer().getPlayerList().getOppedPlayerNames();
+            if (Arrays.asList(admins).contains(event.player.getName())){
+                ForgeVersion.CheckResult version = ForgeVersion.getResult(Loader.instance().activeModContainer());
+                if (version.status == ForgeVersion.Status.OUTDATED){
+                    TextComponentString url = new TextComponentString(TextFormatting.DARK_AQUA + TextFormatting.BOLD.toString() + "New update available !");
+                    url.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, Reference.updateURL));
+                    url.getStyle().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentString("Open link")));
+                    event.player.sendMessage(new TextComponentString(TextFormatting.DARK_RED + "["+Reference.MOD_NAME+"] : ").appendSibling(url));
+                }
             }
         }
     }
--- src/main/java/com/vandendaelen/automessagedisplayer/utils/Reference.java
+++ src/main/java/com/vandendaelen/automessagedisplayer/utils/Reference.java
@@ -4,12 +4,13 @@ public class Reference {
     public static final String MOD_ID = "amd";
     public static final String MOD_NAME = "AutoMessageDisplayer";
     public static final String DEP = "required-after:forge@[14.23.4.2807,)";
+    public static final String updateURL = "https://minecraft.curseforge.com/projects/automessagedisplayer";
 
     public static class VERSION{
         public static final String MCVERSION = "1.12.2";
         public static final String MAJORMOD = "0";
         public static final String MAJORAPI = "0";
-        public static final String MINOR = "0";
+        public static final String MINOR = "1";
         public static final String PATCH = "2";
         public static final String VERSION = MCVERSION+"-"+MAJORMOD+"."+MAJORAPI+"."+MINOR+"."+PATCH;
     }
