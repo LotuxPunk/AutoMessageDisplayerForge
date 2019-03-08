83e6815721d7b017c3def071465f33573d8fc447
LotuxPunk <vandendaelenclement@gmail.com>
2019-03-09 00:02:01 +0100

    config

--- src/main/java/com/vandendaelen/automessagedisplayer/AutoMessageDisplayer.java
+++ src/main/java/com/vandendaelen/automessagedisplayer/AutoMessageDisplayer.java
@@ -1,30 +1,22 @@
 package com.vandendaelen.automessagedisplayer;
 
+import com.vandendaelen.automessagedisplayer.config.AMDConfig;
 import com.vandendaelen.automessagedisplayer.utils.Reference;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(Reference.MOD_ID)
 public class AutoMessageDisplayer {
     public static final Logger LOGGER = LogManager.getLogger();
+    public static AutoMessageDisplayer INSTANCE;
 
     public AutoMessageDisplayer() {
-
+        INSTANCE = this;
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, AMDConfig.CONFIG_SPEC);
         MinecraftForge.EVENT_BUS.register(this);
     }
-
-    //    @Mod.EventHandler
-//    public void preInit(FMLPreInitializationEvent event)
-//    {
-//        logger = event.getModLog();
-//    }
-//
-//    @Mod.EventHandler
-//    public void serverStarted(FMLServerStartedEvent event){
-//        AMDEventHandler.END_COUNTDOWN = AMDConfig.GENERAL.minutes * 60 * 20;
-//        logger.info(AMDEventHandler.END_COUNTDOWN);
-//    }
-
 }
--- src/main/java/com/vandendaelen/automessagedisplayer/config/AMDConfig.java
+++ src/main/java/com/vandendaelen/automessagedisplayer/config/AMDConfig.java
@@ -1,35 +1,75 @@
 package com.vandendaelen.automessagedisplayer.config;
 
-import com.vandendaelen.automessagedisplayer.utils.Reference;
-import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.ForgeConfigSpec;
+import org.apache.commons.lang3.tuple.Pair;
 
-@Config(modid = Reference.MOD_ID, name = Reference.MOD_NAME)
-public class AMDConfig {
-    public static final General GENERAL = new General();
+public class AMDConfig{
+    public static final AMDConfig CONFIG;
+    public static final ForgeConfigSpec CONFIG_SPEC;
+    
+    public final ForgeConfigSpec.IntValue timer;
+    public final ForgeConfigSpec.ConfigValue<String[]> messages;
+    public final ForgeConfigSpec.ConfigValue<String> prefix;
+    public final ForgeConfigSpec.BooleanValue enablePrefix;
+    public final ForgeConfigSpec.BooleanValue updateChecker;
+    public final ForgeConfigSpec.BooleanValue random;
+    
+    static {
+        Pair<AMDConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(AMDConfig::new);
+        CONFIG_SPEC = specPair.getRight();
+        CONFIG = specPair.getLeft();
+    }
 
-    public static class General{
-        @Config.LangKey("amd.config.minutes")
-        @Config.Comment("Minutes between 2 messages")
-        public int minutes = 30;
+    public AMDConfig(ForgeConfigSpec.Builder builder) {
+        builder.push("general");
+        timer = builder
+                .comment("Minutes between 2 messages")
+                .translation("amd.config.minutes")
+                .defineInRange("timer",30, 1, Integer.MAX_VALUE);
+        messages = builder
+                .comment("Messages to displays")
+                .translation("amd.config.messages")
+                .define("messages",new String[]{"Thanks for downloading AutoMessageDisplayer", "Cake or pie ?"});
+        prefix = builder
+                .comment("Prefix of the message")
+                .translation("amd.config.prefix")
+                .define("prefix","AutoMessageDisplayer");
+        enablePrefix = builder
+                .comment("Enable prefix before the message ?")
+                .translation("amd.config.enable.prefix")
+                .define("enable_prefix", true);
+        updateChecker = builder
+                .comment("Check if an update exist ?")
+                .translation("amd.config.update")
+                .define("update_checker", true);
+        random = builder
+                .comment("Display messages randomly")
+                .translation("amd.config.random")
+                .define("random",false);
+        builder.pop();
+    }
 
-        @Config.LangKey("amd.config.messages")
-        @Config.Comment("Messages to displays")
-        public String[] messages = {"Thanks for downloading AutoMessageDisplayer", "Cake or pie ?"};
+    public int getTimer() {
+        return CONFIG.timer.get();
+    }
 
-        @Config.LangKey("amd.config.prefix")
-        @Config.Comment("Prefix of the message")
-        public String prefix = "AutoMessageDisplayer";
+    public String[] getMessages() {
+        return CONFIG.messages.get();
+    }
 
-        @Config.LangKey("amd.config.enable.prefix")
-        @Config.Comment("Enable prefix before the message ?")
-        public boolean enablePrefix = true;
+    public String getPrefix() {
+        return CONFIG.prefix.get();
+    }
 
-        @Config.LangKey("amd.config.update")
-        @Config.Comment("Check if an update exist ?")
-        public boolean updateChecker = true;
+    public boolean getEnablePrefix() {
+        return CONFIG.enablePrefix.get();
+    }
 
-        @Config.LangKey("amd.config.random")
-        @Config.Comment("Display messages randomly")
-        public boolean random = false;
+    public boolean getUpdateChecker() {
+        return CONFIG.updateChecker.get();
+    }
+
+    public boolean getRandom() {
+        return CONFIG.random.get();
     }
 }
