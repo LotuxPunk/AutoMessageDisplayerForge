614ef7dd98be8f3b8bad9a98005061840b5528bc
LotuxPunk <clement@vandendaelen.com>
2019-07-10 14:07:45 +0200

    port to 1.14.3

--- src/main/java/com/vandendaelen/automessagedisplayer/utils/PlayerHelper.java
+++ src/main/java/com/vandendaelen/automessagedisplayer/utils/PlayerHelper.java
@@ -1,19 +1,19 @@
 package com.vandendaelen.automessagedisplayer.utils;
 
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.util.text.TextComponentString;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
 
 import java.util.List;
 
 public class PlayerHelper {
-    public static void sendMessage(EntityPlayerMP player, String message){
-        player.sendMessage(new TextComponentString(message));
+    public static void sendMessage(ServerPlayerEntity player, String message){
+        player.sendMessage(new StringTextComponent(message));
     }
 
     public static void sendMessageToAllPlayer(String message){
-        List<EntityPlayerMP> players = ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayers();
-        for(EntityPlayerMP playerMP : players){
+        List<ServerPlayerEntity> players = ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayers();
+        for(ServerPlayerEntity playerMP : players){
             sendMessage(playerMP, message);
         }
     }
